00c19f52180e6aa725eb75df2f106f346eb18120
molot360 <77602767+molot360@users.noreply.github.com>
2021-02-18 20:39:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.use(express.static(__dirname + '/'));
 app.get('*', (req, res) =>{
 res.sendFile(path.resolve(__dirname, './src/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+app.listen(port);
